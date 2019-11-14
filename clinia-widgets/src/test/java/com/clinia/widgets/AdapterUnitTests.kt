27fde7dc0079be6b28bcfd845726f2f8e140d95b
capitainecl <guillaume.poirier@clinia.ca>
2019-11-14 14:00:19 -0500

    fix: Renamed unit tests

--- clinia-widgets/src/test/java/com/clinia/widgets/AdapterUnitTests.kt
+++ clinia-widgets/src/test/java/com/clinia/widgets/AdapterUnitTests.kt
@@ -13,7 +13,7 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+class AdapterUnitTests {
     @Test
     fun singleIndexAdapter_toJson_urlEncode() {
         val adapter = SingleIndexAdapter()
