e6d306f16c5f0fa0c0bf6e4c1480c42924a07ef9
Etienne Soulard-Geoffrion <etienne.soulard-geoffrion@clinia.ca>
2019-11-14 16:05:18 -0500

    inf: added script files for build & release

---
+++ README.md
@@ -0,0 +1,36 @@
+# Clinia Search API Client for Android
+
+## Contributing
+
+[Your contributions](https://github.com/clinia/cliniasearch-client-android/pull/new) are welcome! Please use our [formatting configuration](https://github.com/clinia/CodingStyle#android) to keep the coding style consistent.
+
+## API Documentation
+
+1. **[Contributing](#contributing)**
+
+
+1. **[Install](#install)**
+
+
+1. **[Quick Start](#quick-start)**
+
+
+1. **[Search](#search)**
+
+## Install
+
+Install the Android client by adding the following dependency to your `Gradle` build file.
+
+```gradle
+dependencies {
+    implementation 'ca.clinia:cliniasearch-android:1.+'
+}
+```
+
+## Quick Start
+
+TODO
+
+## Getting Help
+
+- **Found a bug?** You can open a [GitHub issue](https://github.com/clinia/cliniasearch-client-android/issues).
---
+++ run_test.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+my_travis_retry() {
+  local result=0
+  local count=1
+  while [ $count -le 3 ]; do
+      [ $result -ne 0 ] && {
+        echo -e "\n${ANSI_RED}The command \"$@\" failed. Retrying, $count of 3.${ANSI_RESET}\n" >&2
+      }
+      "$@"
+      result=$?
+      [ $result -eq 0 ] && break
+      count=$((count + 1))
+      sleep 1
+  done
+  [ $count -gt 3 ] && {
+    echo -e "\n{ANSI_RED}The command \"$@\" failed 3 times.${ANSI_RESET}\n" >&2
+  }
+  return $result
+}
+
+if [ "$TRAVIS" = "true" ]; then
+    if [ -n "$(type -t travis_retry)" ] && [ "$(type -t travis_retry)" = function ]; then
+        RETRY="travis_retry"
+    else
+        # travis_retry is undefined in subshells. the recommended way to handle this
+        # is to "include the function code directly", see following conversation
+        # Conversation:  https://twitter.com/plexus/status/499194992632811520
+        # Source: https://git.io/vV5md
+        RETRY="my_travis_retry"
+    fi
+fi
+
+if ! [[ $TRAVIS_JOB_NUMBER && ${TRAVIS_JOB_NUMBER-_} ]]; then
+    echo "/!\ TRAVIS_JOB_NUMBER is not set."
+    TRAVIS_JOB_NUMBER=$RANDOM.$RANDOM
+fi
+
+echo "Running Android test..."
+$RETRY ./gradlew testRelease
